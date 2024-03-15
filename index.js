ba026cb343635a0509c0dec9709d26a44cb12dfb
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-03-15 13:07:11 +0600

    added post and get routes for each conversations

--- index.js
+++ index.js
@@ -26,6 +26,66 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const userCollection = client.db('ChatPal').collection('users')
+        const conversationCollection = client.db('ChatPal').collection('conversations')
+
+        // register user
+        app.post('/api/register', async (req, res) => {
+            try {
+                const userInfo = req.body;
+                const result = await userCollection.insertOne(userInfo)
+                res.send(result)
+            } catch (error) {
+                console.log(error, 'error')
+            }
+        })
+        app.post('/api/login', async (req, res) => {
+            try {
+                const userInfo = req.body;
+                const result = await userCollection.insertOne(userInfo)
+                res.send(result)
+            } catch (error) {
+                console.log(error, 'error')
+            }
+        })
+
+        // for users
+        app.post('/api/conversations', async (req, res) => {
+            try {
+                const { senderId, receiverId } = req.body
+                const newConversation = await conversationCollection.insertOne({ members: [senderId, receiverId] })
+                res.send(newConversation)
+            } catch (error) {
+                console.log(error, 'error')
+            }
+        })
+
+        app.get('/api/conversations', async (req, res) => {
+            try {
+                const result = await conversationCollection.find().toArray()
+                res.send(result)
+            } catch (error) {
+                console.log(error, 'error')
+            }
+        })
+
+        app.get('/api/conversations/:userId', async (req, res) => {
+            try {
+                const userId = req.params.userId
+                const conversations = { members: { $in: [userId] } }
+                const conversationUserData = conversations.map(async(conversation)=>{
+                    const receiverId = conversation.find((member)=> member !== userId)
+                    return await conversationCollection.findOne( { members : receiverId})
+                }) 
+                const result = await conversationCollection.findOne(conversationUserData)
+                res.send(result)
+            } catch (error) {
+                console.log(error, 'error')
+            }
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
