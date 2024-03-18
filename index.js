010a6e6299ddb0d1e1b23eaef3596adb8bb5be71
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-03-18 15:06:17 +0600

    rearranged the server with mongoose

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 
 app.use(cors())
@@ -13,89 +13,9 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aw2xu1p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-
-        const userCollection = client.db('ChatPal').collection('users')
-        const conversationCollection = client.db('ChatPal').collection('conversations')
-
-        // register user
-        app.post('/api/register', async (req, res) => {
-            try {
-                const userInfo = req.body;
-                const result = await userCollection.insertOne(userInfo)
-                res.send(result)
-            } catch (error) {
-                console.log(error, 'error')
-            }
-        })
-        app.post('/api/login', async (req, res) => {
-            try {
-                const userInfo = req.body;
-                const result = await userCollection.insertOne(userInfo)
-                res.send(result)
-            } catch (error) {
-                console.log(error, 'error')
-            }
-        })
-
-        // for conversations
-        app.post('/api/conversations', async (req, res) => {
-            try {
-                const { senderId, receiverId } = req.body
-                const newConversation = await conversationCollection.insertOne({ members: [senderId, receiverId] })
-                res.send(newConversation)
-            } catch (error) {
-                console.log(error, 'error')
-            }
-        })
-
-        app.get('/api/conversations', async (req, res) => {
-            try {
-                const result = await conversationCollection.find().toArray()
-                res.send(result)
-            } catch (error) {
-                console.log(error, 'error')
-            }
-        })
-
-        app.get('/api/conversations/:userId', async (req, res) => {
-            try {
-                const userId = req.params.userId
-                const singleConversations = await conversationCollection.find({ members: { $in: [userId] } })
-                console.log(singleConversations)
-                const conversationUserData = singleConversations.map(async(conversation)=>{
-                    const receiverId = conversation.find((member)=> member !== userId)
-                    return await conversationCollection.findOne( { members : receiverId})
-                }) 
-                const result = await conversationCollection.findOne(conversationUserData)
-                res.send(result)
-            } catch (error) {
-                console.log(error, 'error')
-            }
-        })
-
-
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
-}
-run().catch(console.dir);
+mongoose.connect(uri)
+.then(()=> console.log('connected to mongoDB'))
+.catch((err)=> console.error('could not connect to mongoDB',err))
 
 
 app.get('/', (req, res) => {
