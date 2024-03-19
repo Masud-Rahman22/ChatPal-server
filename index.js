a582d2bf96131480478917219fc530e3e3e6c54c
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-03-19 19:31:22 +0600

    user registration with bcryptjs for hashed password is completed

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 const Users = require('./models/Users')
+const bcryptjs = require('bcryptjs')
 
 app.use(cors())
 app.use(express.json())
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aw2xu1p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -17,7 +16,27 @@ mongoose.connect(uri)
 .then(()=> console.log('connected to mongoDB'))
 .catch((err)=> console.error('could not connect to mongoDB',err))
 
-
+app.post('/api/register', async(req,res,next)=>{
+    const {fullName, email,password} = req.body;
+    if(!fullName || !email || !password){
+        res.status(400).send('please fill all required fields')
+    }
+    else{
+        const isAlreadyExist = await Users.findOne({email})
+        if(isAlreadyExist){
+            res.status(400).send('User already exists')
+        }
+        else{
+            const newUser = new Users({fullName: fullName, email: email})
+            bcryptjs.hash(password, 10, (err,hashedPassword)=>{
+                newUser.set('password', hashedPassword)
+                newUser.save()
+                next()
+            })
+            return res.status(200).send('user registered successfully')
+        }
+    }
+})
 
 app.get('/', (req, res) => {
     res.send('Project is running properly')
